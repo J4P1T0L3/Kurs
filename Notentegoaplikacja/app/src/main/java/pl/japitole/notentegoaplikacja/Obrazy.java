e1e6c3db52be3997b46d9fc5b72c65ce2bc4f008
J4P1T0L3 <ripsteamsupermaarek@gmail.com>
2017-09-24 14:50:38 +0200

    ebe ebe

---
+++ Notentegoaplikacja/app/src/main/java/pl/japitole/notentegoaplikacja/Obrazy.java
@@ -0,0 +1,16 @@
+package pl.japitole.notentegoaplikacja;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by uczen on 2017-09-24.
+ */
+
+public class Obrazy extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logowanie);
+    }
+}
