89152c7692107b2ae4f9e3466aa4c6885a266ccf
J4P1T0L3 <ripsteamsupermaarek@gmail.com>
2017-09-24 11:01:00 +0200

    szatan nooo

--- Notentegoaplikacja/app/src/main/java/pl/japitole/notentegoaplikacja/MainActivity.java
+++ Notentegoaplikacja/app/src/main/java/pl/japitole/notentegoaplikacja/MainActivity.java
@@ -2,12 +2,79 @@ package pl.japitole.notentegoaplikacja;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textEx;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rejejstracja);
+        setContentView(R.layout.movie);
+        textEx = (TextView) findViewById(R.id.text);
+        String napis = textEx.getText().toString();
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        films.add(film);
+        films.add(film2);
+        films.add(film3);
+        films.add(film4);
+        films.add(film5);
+        
+
+        for (int i = 0; i<films.size(); i++){
+            textEx.append("Tytuł " + films.get(i).getTitle() + " oraz chyba ma " + films.get(i).getLenght() + " sezonw" + "\n");
+        }
+
     }
+
+    Movie film = new Movie("Fargo", 3);
+    Movie film2 = new Movie("Preacher", 2);
+    Movie film3 = new Movie("Miasteczko Twin Peaks", 3);
+    Movie film4 = new Movie("iZombie", 3);
+    Movie film5 = new Movie("American Gods", 2);
+
+    LinkedList<Movie> films = new LinkedList<>();
+
+
 }
---
+++ Notentegoaplikacja/app/src/main/java/pl/japitole/notentegoaplikacja/Movie.java
@@ -0,0 +1,31 @@
+package pl.japitole.notentegoaplikacja;
+
+/**
+ * Created by uczen on 2017-09-24.
+ */
+
+public class Movie {
+    private String title;
+    private int lenght;
+
+    public Movie(String title, int lenght) {
+        this.title = title;
+        this.lenght = lenght;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+}
